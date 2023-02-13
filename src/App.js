86296db008ebccbec17e4a266bbbbabc255906d4
Sathish R <107674282+sathish0706@users.noreply.github.com>
2023-02-13 20:45:00 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Signin />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Signin />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/password-reset" element={<ResetPassword />} />
